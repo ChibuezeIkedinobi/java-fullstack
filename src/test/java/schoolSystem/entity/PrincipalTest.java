b5e36128512df3d8fdef4ae1e554ef0db711ef25
Ikedinobi <Ikdinobi@gmail.com>
2024-10-03 11:58:23 +0100

    test

--- src/test/java/schoolSystem/entity/PrincipalTest.java
+++ src/test/java/schoolSystem/entity/PrincipalTest.java
@@ -2,15 +2,22 @@ package schoolSystem.entity;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrincipalTest {
 
-    @Test
-    void expel() {
-    }
+    List<Student> studentList = new ArrayList<>();
+    List<Course> courseList = new ArrayList<>();
+    ClassRoom classRoom = new ClassRoom(studentList, courseList);
+    Principal principal = new Principal("Ikedinobi","Principal",  5400000, classRoom);
+
 
     @Test
     void admit() {
+        Applicant applicant1 = new Applicant("John", 24);
+        assertTrue(principal.admit(applicant1));
     }
 }
